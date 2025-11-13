63981c157ac41cd1141dca5ba8a4eb5814e79381
Enje <enje.mztv@gmail.com>
2025-11-13 21:11:22 +0300

    fixed

--- test/test.py
+++ test/test.py
@@ -1,10 +1,12 @@
 import requests
 
 def test_add():
+    """Описание теста"""
     body = {"title":"enjetest","completed":False}
     response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
     id = response.json()["id"]
     print("id1", id)
+
 
     body = {"title":"enjetest1"}
     response = requests.patch(f'https://todo-app-sky.herokuapp.com/{id}', json=body)
